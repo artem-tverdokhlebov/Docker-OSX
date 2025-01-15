3e5915e95ed60f6f2416ddb12973698c93cc8c16
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-15 13:47:14 +0100

    Update start-wireguard.sh

--- start-wireguard.sh
+++ start-wireguard.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+
 # Create and configure the TAP device for QEMU
 sudo ip tuntap add dev tap0 mode tap user $(whoami) 2>/dev/null || true
 sudo ip link set tap0 up
 sudo ip addr add 192.168.100.1/24 dev tap0
 sudo ip route add 192.168.100.0/24 dev tap0
 
+# Add NAT for QEMU traffic from tap0 through WireGuard
+sudo iptables -t nat -A POSTROUTING -o wg0 -s 192.168.100.0/24 -j MASQUERADE
+sudo iptables -A FORWARD -i tap0 -o wg0 -j ACCEPT
+sudo iptables -A FORWARD -i wg0 -o tap0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 # Stop WireGuard if running
 sudo wg-quick down /etc/wireguard/wg0.conf 2>/dev/null || true
 
 # Start WireGuard
 sudo wg-quick up /etc/wireguard/wg0.conf
-
-# Add NAT for QEMU traffic from tap0 through WireGuard
-sudo iptables -t nat -A POSTROUTING -o wg0 -s 192.168.100.0/24 -j MASQUERADE
-sudo iptables -A FORWARD -i tap0 -o wg0 -j ACCEPT
-sudo iptables -A FORWARD -i wg0 -o tap0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 sudo dnsmasq --no-daemon --conf-file=/etc/dnsmasq.conf &
 DNSMASQ_PID=$!
