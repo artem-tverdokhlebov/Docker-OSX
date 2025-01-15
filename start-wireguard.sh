65444b72aa8ac9b858015b2ab2a7a9d848f34157
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-15 15:03:45 +0100

    Update start-wireguard.sh

--- start-wireguard.sh
+++ start-wireguard.sh
@@ -19,15 +19,18 @@ sudo ip route show
 sudo iptables -t nat -L -v -n
 sudo wg show
 
+sudo dnsmasq --no-daemon --conf-file=/etc/dnsmasq.conf &
+DNSMASQ_PID=$!
+
 sudo ip tuntap add dev tap0 mode tap user $(whoami)
 sudo ip link set tap0 up
 sudo ip addr add 192.168.100.1/24 dev tap0
 
-# Test connectivity to an external IP
-ping -c 4 8.8.8.8
-
 # Check public IP (should match the WireGuard server's IP)
 curl --interface tap0 -v ifconfig.me
+
+# Test connectivity to an external IP
+ping -c 4 8.8.8.8
 
 # Execute CMD
 exec "tail /dev/null"
