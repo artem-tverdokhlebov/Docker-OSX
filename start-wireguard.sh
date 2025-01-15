322febb96cef3ce0646f654b1dddc57a192cc6d7
artem-tverdokhlebov <artem.tverdokhlebov@gmail.com>
2025-01-15 11:53:28 +0100

    Update start-wireguard.sh

--- start-wireguard.sh
+++ start-wireguard.sh
@@ -4,10 +4,10 @@
 wg-quick up /etc/wireguard/wg0.conf
 
 # Route all traffic through WireGuard except VNC
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
-iptables -A FORWARD -i wg0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 5900:5999 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+sudo iptables -A FORWARD -i wg0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 5900:5999 -j ACCEPT
 
 # Start Docker-OSX services
 exec "$@"
